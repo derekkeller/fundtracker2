8f7a02dd1af43f557489a70ef2f456490a23f630
Derek Keller <derekbkeller@gmail.com>
2011-10-12 12:50:20 -0600

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Fundtracker2::Application.config.secret_token = '50834f777f13e3bcb517a275e0c772fba951561aa9c272eb981e0eb98fcb3abf48ff5afed9b521a7483d7047f552b1a232fc1bbb1700a31734f251a4534cf8b1'
